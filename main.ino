1168313a364c104009e1d281f977978df3239110
Angels-dev <zachary.guenot@gmail.com>
2021-05-18 11:31:16 +0200

    Saved

--- main.ino
+++ main.ino
@@ -1,22 +1,39 @@
 const int lightSensorPin=5;
 const int waterSensorPin=6;
 const int atmosSensorPin=7;
-const int SensorPin=8;
+const int windSensorPin=8;
 
+int lightSensorValue;
+int waterSensorValue;
+int atmosSensorValue;
+int windSensorValue;
 
 void setup()
 {
     pinMode(lightSensorPin,INPUT);
     pinMode(waterSensorPin,INPUT);
     pinMode(atmosSensorPin,INPUT);
-    pinMode(SensorPin,INPUT);
-    Serial.begin(9600);
+    pinMode(windSensorPin,INPUT);
+    Serial.begin(1000);
 }
 
 void loop()
 {
-    digitalWrite(2,HIGH);
-    delay(100);
-    digitalWrite(2,LOW);
-    delay(100);
+    lightSensorValue=analogRead(lightSensorPin);
+    Serial.write("Light Sensor Value : ");
+    Serial.write(lightSensorValue);
+
+    waterSensorValue=analogRead(waterSensorPin);
+    Serial.write("Ground humidity Sensor Value : ");
+    Serial.write(waterSensorValue);
+
+    atmosSensorValue=analogRead(atmosSensorPin);
+    Serial.write("Atmospheric pressure Sensor Value : ");
+    Serial.write(atmosSensorValue);
+
+    windSensorValue=analogRead(windSensorPin);
+    Serial.write("Wind Sensor Value : ");
+    Serial.write(windSensorValue);
+
+    delay(1000);
 }
